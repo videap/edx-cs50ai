cd06219e76c45072542b12b57033d229c0bb79d6
Vitor Pinto <vitor@nursa.com>
2023-10-02 22:00:56 -0300

    feat: implement player function

--- tictactoe.py
+++ tictactoe.py
@@ -22,7 +22,23 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    sum_of_board = 0
+    for line in board:
+        for i in line:
+            if i:
+                sum_of_board += 1
+
+    #sum of board is odd -> turn of O
+    #sum of board is even -> turn of X
+    #sum of board is 9 -> Raise error "Game is Over"
+
+    if sum_of_board == 9:
+        raise Exception("Game is Over")
+
+    remainder = sum_of_board % 2
+
+    return "X" if remainder == 0 else "O"
+
 
 
 def actions(board):
@@ -65,3 +81,9 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     raise NotImplementedError
+
+
+if __name__ == "__main__":
+
+    initial = initial_state()
+    print(player(initial))
